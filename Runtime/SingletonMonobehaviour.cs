2c57bf303198c558bd37c3e838d46fd4d7e59249
Vibas-MacBook Air <gamedev@resurzence.in>
2022-07-03 23:29:54 +0530

    Singleton Monobehaviour added

---
+++ Runtime/SingletonMonobehaviour.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+namespace Lucky4u
+{
+    public class SingletonMonobehaviour<T> : MonoBehaviour where T : SingletonMonobehaviour<T>
+    {
+        private static T _instance;
+
+        public static T instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = (T)FindObjectOfType(typeof(T));
+                    DontDestroyOnLoad(_instance.gameObject);
+                }
+
+                if (_instance == null)
+                {
+                    GameObject singleton = new GameObject();
+                    _instance = singleton.AddComponent<T>();
+                    singleton.name = "(Singleton) " + typeof(T).ToString();
+                    DontDestroyOnLoad(singleton);
+                }
+
+                return _instance;
+            }
+        }
+
+        void Awake()
+        {
+            if (_instance != null)
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+            DontDestroyOnLoad(gameObject);
+            _instance = (T)this;
+        }
+    }
+}
